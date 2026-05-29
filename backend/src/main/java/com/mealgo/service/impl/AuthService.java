1717103d482a20165dfa396b2c49ae05517b7521
sugar025016 <sugar025016@gmail.com>
2026-05-29 19:39:42 +0800

    build: AuthService

--- backend/src/main/java/com/mealgo/service/impl/AuthService.java
+++ backend/src/main/java/com/mealgo/service/impl/AuthService.java
@@ -1,6 +1,5 @@
 package com.mealgo.service.impl;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
